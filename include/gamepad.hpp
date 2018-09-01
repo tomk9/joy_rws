df54f9c075f38a93d06dd91fb582851ccf3aec7a
tomek <tomaszsz95@gmail.com>
2018-09-01 20:27:25 +0200

    ROS init and subscribe to joy_node

--- include/gamepad.hpp
+++ include/gamepad.hpp
@@ -5,22 +5,53 @@
 #include <rw/models/WorkCell.hpp>
 #include <QtGui>
 #include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
 #include <thread>
 #include <chrono>
 
 #include "ui_gamepad.h"
 
+#include <string>
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <linux/input.h>
+
+#define N_EFFECTS 3
+
+void mainLoop();
+void rumble(int i);
+void initRumble();
+
+class InitROS
+{
+  public:
+	InitROS()
+	{
+		int argc = 0;
+		char *argv[] = {'\0'};
+		ros::init(argc, argv, "controller_plugin");
+	}
+};
 
 /**
  * @brief A plugin for testing controller.
  */
-class Controller: public rws::RobWorkStudioPlugin,
-private Ui::gamepad {
+class Controller : public rws::RobWorkStudioPlugin,
+				   private Ui::gamepad,
+				   InitROS
+{
 	Q_OBJECT
 	Q_PLUGIN_METADATA(IID "gamepad" FILE "joy_rws.json")
 	Q_INTERFACES(rws::RobWorkStudioPlugin)
 
-public:
+  public:
 	//! @brief constructor
 	Controller();
 
@@ -28,7 +59,7 @@ public:
 	virtual ~Controller();
 
 	//! @copydoc rws::RobWorkStudioPlugin::open(rw::models::WorkCell* workcell)
-	virtual void open(rw::models::WorkCell* workcell);
+	virtual void open(rw::models::WorkCell *workcell);
 
 	//! @copydoc rws::RobWorkStudioPlugin::close()
 	virtual void close();
@@ -42,22 +73,29 @@ public:
 	 * @brief listen for generic event
 	 * @param event [in] the event id
 	 */
-	void genericEventListener(const std::string& event);
+	void genericEventListener(const std::string &event);
 
 	//! @brief listen for key events
 	void keyEventListener(int key, Qt::KeyboardModifiers modifier);
 
-private slots:
-void ObslugaPrzyciskuConnect();
+	void joyCallback(const sensor_msgs::Joy::ConstPtr &joy);
 
-private:
-  /* plugin interface widget */
+  private slots:
+	void ObslugaPrzyciskuConnect();
+
+  private:
+	/* plugin interface widget */
 	Ui::gamepad _ui;
 
 	std::thread mainLoopThread;
-  
-  rw::models::WorkCell* _wc;
-  rw::kinematics::State _state;
 
-//   RobotInterface* robotUR5;
+	rw::models::WorkCell *_wc;
+	rw::kinematics::State _state;
+	ros::NodeHandle nh_;
+	ros::Subscriber joy_sub_;
+
+	double a_scale_ = 1;
+	double twist[5];
+
+	//   RobotInterface* robotUR5;
 };
--- src/gamepad.cpp
+++ src/gamepad.cpp
@@ -10,65 +10,71 @@ using namespace rw::models;
 using namespace rw::kinematics;
 using namespace boost;
 
-void mainLoop(){
-    while(true){
-        Log::infoLog() << "loop" << endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+Controller::Controller() : RobWorkStudioPlugin("Controller", QIcon(":/gamepad.png"))
+{
+    _ui.setupUi(this);
+
+    connect(_ui.pushButton_2, SIGNAL(clicked()), this, SLOT(ObslugaPrzyciskuConnect()));
+
+    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &Controller::joyCallback, this);
+    initRumble();
+    mainLoopThread = std::thread(mainLoop);
+    // ros::spin();
+    // Log::infoLog() << "Konstruktor";
+}
+
+Controller::~Controller()
+{
+}
+
+void Controller::initialize()
+{
+
+    //getRobWorkStudio()->genericEvent().add(boost::bind(&RWSPlugin::genericEventListener, this, _1), this);
+    //getRobWorkStudio()->keyEvent().add(boost::bind(&RWSPlugin::keyEventListener, this, _1, _2), this);
+
+    log().setLevel(Log::Info);
+    //   robotUR5 = new RobotInterface();
+    //  Log::infoLog() << "Inicjalizacja";
+}
+
+void Controller::open(WorkCell *workcell)
+{
+
+    try
+    {
+        _wc = workcell;
+        _state = getRobWorkStudio()->getState();
     }
+    catch (const rw::common::Exception &e)
+    {
+        QMessageBox::critical(NULL, "RW Exception", e.what());
+    }
+    // Log::infoLog() << "Open";
 }
 
-Controller::Controller() :
-  RobWorkStudioPlugin("Controller", QIcon(":/gamepad.png")) {
-  _ui.setupUi(this);
-  
-  connect(_ui.pushButton_2, SIGNAL(clicked()), this, SLOT(ObslugaPrzyciskuConnect()));
-  int argc = 0;
-  char* argv[] = {'\0'};
-  ros::init(argc, argv, "sts_plugin");
-  mainLoopThread = std::thread(mainLoop);
+void Controller::close()
+{
+    /* ... */
 }
 
-Controller::~Controller() {
+void Controller::update()
+{
+    /* ... */
 }
 
-void Controller::initialize() {
-  
-  //getRobWorkStudio()->genericEvent().add(boost::bind(&RWSPlugin::genericEventListener, this, _1), this);
-  //getRobWorkStudio()->keyEvent().add(boost::bind(&RWSPlugin::keyEventListener, this, _1, _2), this);
-  
-  log().setLevel(Log::Info);
-//   robotUR5 = new RobotInterface();
-}
+void Controller::genericEventListener(const std::string &event)
+{
 
-void Controller::open(WorkCell* workcell) {
-  
-  try {
-      _wc = workcell;
-      _state = getRobWorkStudio()->getState();
-      
-  } catch (const rw::common::Exception& e) {
-      QMessageBox::critical(NULL, "RW Exception", e.what());
-  }
-}
-
-void Controller::close() {
-  /* ... */
-}
-
-void Controller::update() {
-  /* ... */
-}
-
-void Controller::genericEventListener(const std::string & event) {
-
-  /*if (event == "DynamicWorkCellLoaded") {
+    /*if (event == "DynamicWorkCellLoaded") {
     
   }*/
 }
 
-void Controller::keyEventListener(int key, Qt::KeyboardModifiers modifier) {
-  
-  /* ... */
+void Controller::keyEventListener(int key, Qt::KeyboardModifiers modifier)
+{
+
+    /* ... */
 }
 
 // void Controller::ObslugaPrzycisku(){
@@ -79,13 +85,13 @@ void Controller::keyEventListener(int key, Qt::KeyboardModifiers modifier) {
 //   // rw::math::Q q = _robot->getQ(_state);
 //   // Log::infoLog() << q << endl;
 //   // rw::math::Q q1 = rw::math::Q(6, 2.3, -1.1, 1.0, 1.3, 1.1, 1.0);
-  
+
 //   // _robot->setQ(q1, _state);
 //   // getRobWorkStudio()->setState(_state);
 //   rwhw::URRTData data = robotUR5->getData();
 //   rw::math::Q q = data.qActual;
 //         rw::math::Q dq = data.dqActual;
-        
+
 //         if (q.size() == 6) {
 //         _robot->setQ(q, _state);
 //         getRobWorkStudio()->setState(_state);
@@ -94,10 +100,134 @@ void Controller::keyEventListener(int key, Qt::KeyboardModifiers modifier) {
 
 // }
 
-void Controller::ObslugaPrzyciskuConnect(){
-  Log::infoLog() << "Connecting..." << endl;
-//   if (robotUR5->connect()) {
-//     Log::infoLog() << "Connected" << endl;
-//   }
+void Controller::ObslugaPrzyciskuConnect()
+{
+    Log::infoLog() << "Connecting..." << endl;
+    //   if (robotUR5->connect()) {
+    //     Log::infoLog() << "Connected" << endl;
+    //   }
+}
+void Controller::joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
+{
+    Log::infoLog() << "Callback..." << endl;
+    for (int i = 0; i < 5; ++i)
+    {
+        Controller::twist[i] = Controller::a_scale_ * joy->axes[i];
+        Log::infoLog() << Controller::twist[i] << endl;
+    }
 }
 
+void mainLoop()
+{
+    while (true)
+    {
+        Log::infoLog() << "loop" << endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        ros::spinOnce();
+    }
+}
+
+struct ff_effect effects[N_EFFECTS];
+struct input_event play, stop, gain;
+int fd;
+
+void rumble(int i)
+{
+    memset(&play, 0, sizeof(play));
+    play.type = EV_FF;
+    play.code = effects[i].id;
+    play.value = 1;
+
+    if (write(fd, (const void *)&play, sizeof(play)) == -1)
+    {
+        perror("Play effect");
+        exit(1);
+    }
+    // memset(&stop,0,sizeof(stop));
+    // stop.type = EV_FF;
+    // stop.code =  effects[i].id;
+    // stop.value = 0;
+
+    // if (write(fd, (const void*) &stop, sizeof(stop)) == -1) {
+    //     perror("");
+    //     exit(1);
+    // }
+}
+
+void initRumble()
+{
+    const char *device_file_name = "/dev/input/by-id/usb-Microsoft_Controller_3032363030313236363036373439-event-joystick";
+    fd = open(device_file_name, O_RDWR);
+    if (fd == -1)
+    {
+        perror("Open device file");
+        exit(1);
+    }
+    /* download a periodic sinusoidal effect */
+    memset(&effects[0], 0, sizeof(effects[0]));
+    effects[0].type = FF_PERIODIC;
+    effects[0].id = -1;
+    effects[0].u.periodic.waveform = FF_SINE;
+    effects[0].u.periodic.period = 100;       /* 0.1 second */
+    effects[0].u.periodic.magnitude = 0x7fff; /* 0.5 * Maximum magnitude */
+    effects[0].u.periodic.offset = 0;
+    effects[0].u.periodic.phase = 0;
+    effects[0].direction = 0x4000; /* Along X axis */
+    effects[0].u.periodic.envelope.attack_length = 1000;
+    effects[0].u.periodic.envelope.attack_level = 0x7fff;
+    effects[0].u.periodic.envelope.fade_length = 1000;
+    effects[0].u.periodic.envelope.fade_level = 0x7fff;
+    effects[0].trigger.button = 0;
+    effects[0].trigger.interval = 0;
+    effects[0].replay.length = 20000; /* 20 seconds */
+    effects[0].replay.delay = 1000;
+
+    printf("Uploading effect #0 (Periodic sinusoidal) ... ");
+    fflush(stdout);
+    if (ioctl(fd, EVIOCSFF, &effects[0]) == -1)
+    {
+        perror("Error:");
+    }
+    else
+    {
+        printf("OK (id %d)\n", effects[0].id);
+    }
+
+    /* a strong rumbling effect */
+    effects[1].type = FF_RUMBLE;
+    effects[1].id = -1;
+    effects[1].u.rumble.strong_magnitude = 0x8000;
+    effects[1].u.rumble.weak_magnitude = 0;
+    effects[1].replay.length = 500;
+    effects[1].replay.delay = 0;
+
+    printf("Uploading effect #1 (Strong rumble, with heavy motor) ... ");
+    fflush(stdout);
+    if (ioctl(fd, EVIOCSFF, &effects[1]) == -1)
+    {
+        perror("Error");
+    }
+    else
+    {
+        printf("OK (id %d)\n", effects[1].id);
+    }
+
+    /* a weak rumbling effect */
+    effects[2].type = FF_RUMBLE;
+    effects[2].id = -1;
+    effects[2].u.rumble.strong_magnitude = 0;
+    effects[2].u.rumble.weak_magnitude = 0xc000;
+    effects[2].replay.length = 500;
+    effects[2].replay.delay = 0;
+
+    printf("Uploading effect #2 (Weak rumble, with light motor) ... ");
+    fflush(stdout);
+    if (ioctl(fd, EVIOCSFF, &effects[2]) == -1)
+    {
+        perror("Error");
+    }
+    else
+    {
+        printf("OK (id %d)\n", effects[2].id);
+    }
+}
